33d5f5b8be5f6e1955415de2e38f5af731bac777
Ivan45634 <67547747+Ivan45634@users.noreply.github.com>
2023-04-18 01:50:51 +0300

    Доделаны тесты для различных операндов + str метод + проверки исходных списков

--- 03/custom_list.py
+++ 03/custom_list.py
@@ -8,8 +8,9 @@ class CustomList(list):
     +, -, ==, !=, >, >=, <, <=
     """
 
-    def __init__(self, lst):
-        super().__init__(lst)
+    def __init__(self, data=None):
+        super().__init__()
+        self.list = data or []
 
     def __add__(self, other):
         if len(self) == len(other):
@@ -18,8 +19,6 @@ class CustomList(list):
             result = [x + y for x, y in zip(self, other[: len(self)])]
             for i in range(len(self), len(other)):
                 result.append(other[i])
-        else:
-            result = other + self
         return CustomList(result)
 
     def __radd__(self, other):
@@ -28,31 +27,32 @@ class CustomList(list):
     def __sub__(self, other):
         if len(self) == len(other):
             result = [x - y for x, y in zip(self, other)]
-        elif len(self) > len(other):
-            result = [x - y for x, y in zip(self, other)]
-            result.extend(self[len(other):])
         else:
             result = [x - y for x, y in zip(self, other[: len(self)])]
             result.extend([-i for i in other[len(self):]])
         return CustomList(result)
 
     def __rsub__(self, other):
-        return [-i for i in self.__sub__(other)]
+        return CustomList([-i for i in self.__sub__(other)])
+
+    def __str__(self):
+        summa = sum(i for i in self.list if isinstance(i, int))
+        return f"{self.list}, sum={summa}"
 
     def __eq__(self, other):
-        return sum(self) == sum(other)
+        return sum(self.list) == sum(other.list)
 
     def __ne__(self, other):
-        return sum(self) != sum(other)
+        return sum(self.list) != sum(other.list)
 
     def __gt__(self, other):
-        return sum(self) > sum(other)
+        return sum(self.list) > sum(other.list)
 
     def __ge__(self, other):
-        return sum(self) >= sum(other)
+        return sum(self.list) >= sum(other.list)
 
     def __lt__(self, other):
-        return sum(self) < sum(other)
+        return sum(self.list) < sum(other.list)
 
     def __le__(self, other):
-        return sum(self) <= sum(other)
+        return sum(self.list) <= sum(other.list)
--- 03/test_custom_list.py
+++ 03/test_custom_list.py
@@ -8,72 +8,221 @@ class TestCustomList(unittest.TestCase):
 
     def setUp(self):
         """Initialization method"""
-        self.custom_list_1 = CustomList([1, 2, 3])
-        self.custom_list_2 = CustomList([4, 5, 6])
-        self.custom_list_3 = CustomList([1, 2, 3])
-        self.custom_list_4 = CustomList([1, 2, 3, 4])
-        self.custom_list_5 = CustomList([1, 2])
+        self.lst1 = CustomList([1, 2, 3, 4])
+        self.lst2 = CustomList([5, 4, 3, 2, 1])
+        self.lst3 = CustomList([3, 0, 2])
+        self.lst4 = CustomList()
+        self.lst5 = [1, 2, 3, 4, 5]
+        self.lst6 = [1, 2, 3]
+        self.lst7 = [1, 2, 3, 4, 5, 6]
+        self.lst8 = CustomList([1, 0, 1, 0])
+        self.lst9 = CustomList([0, 1, 0, 1, 0])
 
-    def test_add_equal_length_lists(self):
-        """Test case for custom lists with equal length"""
-        result = self.custom_list_1 + self.custom_list_2
-        self.assertEqual(list(result), list(CustomList([5, 7, 9])))
+    def test_addition(self):
+        """
+        test cases for addition
+        """
+        # Same size CustomLists
+        temp_lst1 = self.lst1.copy()
+        temp_lst2 = self.lst2.copy()
+        temp_lst8 = self.lst8.copy()
+        temp_lst9 = self.lst9.copy()
 
-    def test_add_first_list_shorter(self):
-        """Test case for
-        short length custom list and long length custom list"""
-        result = self.custom_list_1 + self.custom_list_4
-        self.assertEqual(list(result), list(CustomList([2, 4, 6, 4])))
+        self.assertEqual(list(self.lst1 + self.lst8),
+                         list(CustomList([2, 2, 4, 4])))
+        self.assertEqual(list(self.lst2 + self.lst9),
+                         list(CustomList([5, 5, 3, 3, 1])))
 
-    def test_add_second_list_shorter(self):
-        """Test case for
-        long length custom list and short length custom list"""
-        result = self.custom_list_4 + self.custom_list_1
-        self.assertEqual(list(result), list(CustomList([2, 4, 6, 4])))
+        self.assertEqual(list(self.lst1), list(temp_lst1))
+        self.assertEqual(list(self.lst2), list(temp_lst2))
+        self.assertEqual(list(self.lst8), list(temp_lst8))
+        self.assertEqual(list(self.lst9), list(temp_lst9))
 
-    def test_subtract_equal_length_lists(self):
-        """Test case for subtract operation for equal length custom list"""
-        result = self.custom_list_2 - self.custom_list_1
-        self.assertEqual(list(result), list(CustomList([3, 3, 3])))
+        # Custom list(len=4) + List(len=5)
+        temp_lst1 = self.lst1.copy()
+        temp_lst5 = self.lst5.copy()
 
-    def test_subtract_first_list_longer(self):
-        """Test case for subtract operation
-        for long length custom list and short length custom list"""
-        result = self.custom_list_4 - self.custom_list_1
-        self.assertEqual(list(result), list(CustomList([0, 0, 0, 4])))
+        self.assertEqual(list(self.lst1 + self.lst5),
+                         list(CustomList([2, 4, 6, 8, 5])))
+        self.assertEqual(list(self.lst5 + self.lst1),
+                         list(CustomList([2, 4, 6, 8, 5])))
 
-    def test_subtract_second_list_longer(self):
-        """Test case for subtract operation
-        for long length custom list and short length custom list"""
-        result = self.custom_list_1 - self.custom_list_5
-        self.assertEqual(list(result), list(CustomList([0, 0, 3])))
+        self.assertEqual(self.lst5, temp_lst5)
+        self.assertEqual(list(self.lst1), list(temp_lst1))
 
-    def test_equal(self):
-        """Test case for subtract operation
-        for long length custom list and short length custom list"""
-        self.assertTrue(self.custom_list_1 == self.custom_list_3)
+        # Custom list + Empty List
+        temp_lst1 = self.lst1.copy()
+        temp_lst4 = self.lst4.copy()
 
-    def test_not_equal(self):
-        """Test case for not equal lists"""
-        self.assertTrue(self.custom_list_1 != self.custom_list_2)
+        self.assertEqual(list(self.lst1 + self.lst4), list(self.lst1))
+        self.assertEqual(list(self.lst4 + self.lst1), list(self.lst1))
 
-    def test_greater_than(self):
-        """Test case for checking operation >"""
-        self.assertTrue(self.custom_list_2 > self.custom_list_1)
+        self.assertEqual(list(self.lst1), list(temp_lst1))
+        self.assertEqual(list(self.lst4), list(temp_lst4))
 
-    def test_greater_than_or_equal(self):
-        """Test case for checking operation >="""
-        self.assertTrue(self.custom_list_2 >= self.custom_list_1)
-        self.assertTrue(self.custom_list_1 >= self.custom_list_3)
+        # Custom list(len=5) + Smaller Custom list(len=3)
+        temp_lst2 = self.lst2.copy()
+        temp_lst3 = self.lst3.copy()
 
-    def test_less_than(self):
-        """Test case for checking operation <"""
-        self.assertTrue(self.custom_list_1 < self.custom_list_2)
+        self.assertEqual(list(self.lst2 + self.lst3),
+                         list(CustomList([8, 4, 5, 2, 1])))
+        self.assertEqual(list(self.lst3 + self.lst2),
+                         list(CustomList([8, 4, 5, 2, 1])))
 
-    def test_less_than_or_equal(self):
-        """Test case for checking operation <="""
-        self.assertTrue(self.custom_list_1 <= self.custom_list_3)
-        self.assertTrue(self.custom_list_1 <= self.custom_list_2)
+        self.assertEqual(list(self.lst2), list(temp_lst2))
+        self.assertEqual(list(self.lst3), list(temp_lst3))
+
+        # Custom list(len=4) + Bigger Custom list(len=5)
+        temp_lst8 = self.lst8.copy()
+        temp_lst9 = self.lst9.copy()
+
+        self.assertEqual(list(self.lst8 + self.lst9),
+                         list(CustomList([1, 1, 1, 1, 0])))
+        self.assertEqual(list(self.lst9 + self.lst8),
+                         list(CustomList([1, 1, 1, 1, 0])))
+
+        self.assertEqual(list(self.lst8), list(temp_lst8))
+        self.assertEqual(list(self.lst9), list(temp_lst9))
+
+        # List + Custom list (same length)
+        temp_lst6 = self.lst6.copy()
+        temp_lst3 = self.lst3.copy()
+
+        self.assertEqual(list(self.lst6 + self.lst3),
+                         list(CustomList([4, 2, 5])))
+        self.assertEqual(list(self.lst3 + self.lst6),
+                         list(CustomList([4, 2, 5])))
+
+        self.assertEqual(self.lst6, temp_lst6)
+        self.assertEqual(list(self.lst3), list(temp_lst3))
+
+        # Larger Custom List (len=5) and Smaller List (len=3)
+        temp_lst2 = self.lst2.copy()
+        temp_lst6 = self.lst6.copy()
+
+        self.assertEqual(list(self.lst2 + self.lst6),
+                         list(CustomList([6, 6, 6, 2, 1])))
+        self.assertEqual(list(self.lst6 + self.lst2),
+                         list(CustomList([6, 6, 6, 2, 1])))
+
+        self.assertEqual(list(self.lst2), list(temp_lst2))
+        self.assertEqual(self.lst6, temp_lst6)
+
+    def test_subtraction(self):
+        """
+        test cases for subtraction
+        """
+        # Same size CustomLists
+        temp_lst1 = self.lst1.copy()
+        temp_lst8 = self.lst2.copy()
+
+        self.assertEqual(list(self.lst1 - self.lst8),
+                         list(CustomList([0, 2, 2, 4])))
+        self.assertEqual(list(self.lst8 - self.lst1),
+                         list(CustomList([0, -2, -2, -4])))
+
+        self.assertEqual(list(self.lst1), list(temp_lst1))
+        self.assertEqual(list(self.lst8), list(temp_lst8))
+
+        # Custom list(len=4) - List(len=5)
+        temp_lst1 = self.lst1.copy()
+        temp_lst5 = self.lst5.copy()
+
+        self.assertEqual(list(self.lst1 - self.lst5),
+                         list(CustomList([0, 0, 0, 0, -5])))
+
+        self.assertEqual(list(self.lst1), list(temp_lst1))
+        self.assertEqual(self.lst5, temp_lst5)
+
+        # Custom list - Empty List
+        temp_lst1 = self.lst1.copy()
+        temp_lst4 = self.lst4.copy()
+
+        self.assertEqual(list(self.lst1 - self.lst4),
+                         list(self.lst1))
+        self.assertEqual(list(self.lst4 - self.lst1),
+                         list(CustomList([-1, -2, -3, -4])))
+
+        self.assertEqual(list(self.lst1), list(temp_lst1))
+        self.assertEqual(list(self.lst4), list(temp_lst4))
+
+        # Custom list(len=3) - Smaller Custom list(len=3)
+        temp_lst2 = self.lst2.copy()
+        temp_lst3 = self.lst3.copy()
+
+        self.assertEqual(list(self.lst2 - self.lst3),
+                         list(CustomList([2, 4, 1, 2, 1])))
+        self.assertEqual(list(self.lst3 - self.lst2),
+                         list(CustomList([-2, -4, -1, -2, -1])))
+
+        self.assertEqual(list(self.lst2), list(temp_lst2))
+        self.assertEqual(list(self.lst3), list(temp_lst3))
+
+        # Custom list (len=4) - Bigger Custom list (len=3)
+        temp_lst3 = self.lst3.copy()
+        temp_lst9 = self.lst9.copy()
+
+        self.assertEqual(list(self.lst3 - self.lst9),
+                         list(CustomList([3, -1, 2, -1, 0])))
+        self.assertEqual(list(self.lst9 - self.lst3),
+                         list(CustomList([-3, 1, -2, 1, 0])))
+
+        self.assertEqual(list(self.lst3), list(temp_lst3))
+        self.assertEqual(list(self.lst9), list(temp_lst9))
+
+        # List (len=3) - Custom list (len=4)
+        temp_lst6 = self.lst6.copy()
+        temp_lst8 = self.lst8.copy()
+
+        self.assertEqual(list(self.lst6 - self.lst8),
+                         list(CustomList([0, 2, 2, 0])))
+        self.assertEqual(list(self.lst8 - self.lst6),
+                         list(CustomList([0, -2, -2, 0])))
+
+        self.assertEqual(self.lst6, temp_lst6)
+        self.assertEqual(list(self.lst8), list(temp_lst8))
+
+        # Larger Custom List (len=5) and Smaller List (len=3)
+        temp_lst2 = self.lst2.copy()
+        temp_lst6 = self.lst6.copy()
+
+        self.assertEqual(list(self.lst2 - self.lst6),
+                         list(CustomList([4, 2, 0, -1, -1])))
+        self.assertEqual(list(self.lst6 - self.lst2),
+                         list(CustomList([-4, -2, 0, 1, 1])))
+
+        self.assertEqual(list(self.lst2), list(temp_lst2))
+        self.assertEqual(self.lst6, temp_lst6)
+
+    def test_comparison(self):
+        """
+        test cases for comparison operations
+        """
+        list1 = CustomList([1, 2, 3])
+        list2 = CustomList([4, 5, 6])
+        list3 = CustomList([6])
+        list4 = CustomList()
+
+        self.assertEqual(list1, list3)
+        self.assertNotEqual(list3, list4)
+        self.assertNotEqual(list1, list2)
+        self.assertNotEqual(list2, list3)
+        self.assertGreater(list1, list4)
+        self.assertGreaterEqual(list1, list3)
+        self.assertGreaterEqual(list2, list3)
+        self.assertLess(list1, list2)
+        self.assertLessEqual(list1, list3)
+        self.assertLessEqual(list1, list2)
+
+    def test_str(self):
+        """
+        test cases for __str__ override
+        """
+        custom_list = CustomList([1, 2, 3])
+        self.assertEqual(str(custom_list), "[1, 2, 3], sum=6")
+        custom_list.append("Hello, World!")
+        self.assertEqual(str(custom_list), "[1, 2, 3], sum=6")
 
 
-unittest.main()
+if __name__ == '__main__':
+    unittest.main()
